51ae84518b46656b3965a794d418d09fe275009a
pedrotoddy271-eng <pedrotoddy271@gmail.com>
2026-02-03 19:50:48 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'jm-financeiro-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
